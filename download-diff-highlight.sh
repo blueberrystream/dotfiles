92feade92b3d71be91d750ca8732da730e992b87
KID the Euforia <kid0725+github@gmail.com>
2014-03-08 16:31:30 +0900

    diff-highlightを配置したあと、実行権を与えるように変更

--- download-diff-highlight.sh
+++ download-diff-highlight.sh
@@ -3,6 +3,7 @@
 if [ -d "$1" ]; then
 	curl -kO https://raw.github.com/git/git/master/contrib/diff-highlight/diff-highlight
 	mv -f diff-highlight "$1/"
+	chmod 755 "$1"/diff-highlight
 else
 	echo usage: download-diff-highlight.sh destination
 fi
