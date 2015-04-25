885e145ee408f004cda61084a54205d364e5e765
KID the Euforia <kid0725+github@gmail.com>
2015-04-25 13:04:08 +0900

    diff-highlight refer to git contrib

--- git-completion-init.sh
+++ git-completion-init.sh
@@ -3,8 +3,17 @@ set -eu
 
 repo_dir=$HOME/Repos/git/github/git
 bash_completion_dir=$HOME/local/etc/bash_completion.d
+local_bin=$HOME/local/bin
 
 git clone git@github.com:git/git.git $repo_dir
 
+if [ ! -d $bash_completion_dir ]; then
+	mkdir -p $bash_completion_dir
+fi
 ln -s $repo_dir/contrib/completion/git-prompt.sh $bash_completion_dir/
 ln -s $repo_dir/contrib/completion/git-completion.bash $bash_completion_dir/
+
+if [ ! -d $local_bin ]; then
+	mkdir -p $local_bin
+fi
+ln -s $repo_dir/contrib/diff-highlight/diff-highlight 
