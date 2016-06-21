62b83865b2c92296b14db6b70eafeaa5b5c88aa0
KID the Euforia <kid0725+github@gmail.com>
2016-06-21 17:35:15 +0900

    fix script

--- scripts/install-git-prepend-user-section.sh
+++ scripts/install-git-prepend-user-section.sh
@@ -6,4 +6,4 @@ local_bin=$HOME/local/bin
 if [ ! -d $local_bin ]; then
   mkdir -p $local_bin
 fi
-ln -s $script_dir/local/bin/git-prepend-user-section $local_bin/
+ln -s $script_dir/../local/bin/git-prepend-user-section $local_bin/
