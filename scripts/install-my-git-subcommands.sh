26f2f9ad001b3ab56a2283db2fb1344cbff437e0
KID the Euforia <kid0725+github@gmail.com>
2017-03-22 12:40:58 +0900

    improve script to put user section to gitconfig

--- scripts/install-my-git-subcommands.sh
+++ scripts/install-my-git-subcommands.sh
@@ -6,4 +6,4 @@ local_bin=$HOME/local/bin
 if [ ! -d $local_bin ]; then
   mkdir -p $local_bin
 fi
-ln -sv $script_dir/../local/bin/git-prepend-user-section $local_bin/
+ln -sv $script_dir/../local/bin/git-put-config-user-section $local_bin/
