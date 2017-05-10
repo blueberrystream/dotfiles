4cdb304359800400326d34c38a2a6e696a6bb99a
KID the Euforia <kid0725+github@gmail.com>
2017-05-10 19:39:37 +0900

    git-completionはLinuxでのみ必要

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+uname=`uname`
 
 $script_dir/install-anyenv.sh
 $script_dir/install-cda.sh
 $script_dir/install-diff-highlight.sh
 $script_dir/install-dircolors.sh
 $script_dir/install-enhancd.sh
-$script_dir/install-git-completion.sh
+[[ $uname == 'Linux' ]] && $script_dir/scripts/install-git-completion.sh
