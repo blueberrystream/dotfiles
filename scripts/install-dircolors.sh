ae698e33096e3a97214a99693f62bd138591fe5f
KID the Euforia <kid0725+github@gmail.com>
2017-06-16 17:56:40 +0900

    format script
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/install-dircolors.sh
+++ scripts/install-dircolors.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install_dir=$HOME/Repos/git/github/dircolors-solarized
+
 if [ ! -d $install_dir ]; then
   rm -f $HOME/dircolors.256dark
   git clone https://github.com/seebi/dircolors-solarized.git $install_dir
