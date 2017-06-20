ddae8f366dded832b2726441398b9cab4bb5c8d1
KID the Euforia <kid0725+github@gmail.com>
2017-06-20 15:20:28 +0900

    remove unnecessary script files
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/update-installed-from-npm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-
-ls -l $(ndenv prefix)/bin | awk '{print $9}' > $script_dir/../local/NPM_GLOBAL_INSTALLED
--- scripts/update-installed-from-source.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-
-ls -l $HOME/local/src | grep ^d | awk '{print $9}' > $script_dir/../local/src/INSTALLED
