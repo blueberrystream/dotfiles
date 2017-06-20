39e333899e6c651873294fa61c746105e4d7c430
KID the Euforia <kid0725+github@gmail.com>
2017-06-20 15:27:52 +0900

    use syndbg/goenv as goenv in anyenv
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/install-anyenv.sh
+++ scripts/install-anyenv.sh
@@ -7,6 +7,7 @@ if [ ! -d $install_dir ]; then
   exec $SHELL -l
   mkdir -p $(anyenv root)/plugins
   git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+  sed -i -e 's@https://github.com/kaneshin/goenv\.git@https://github.com/syndbg/goenv\.git@' "$(ayenv root)/share/anyenv-install/goenv"
 else
   anyenv update
 fi
