49fa713d8c2229f080f326f8040668cc7861b0e6
KID the Euforia <kid0725+github@gmail.com>
2017-06-20 17:22:46 +0900

    fix typo
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/install-anyenv.sh
+++ scripts/install-anyenv.sh
@@ -6,7 +6,7 @@ if [ ! -d $install_dir ]; then
   git clone https://github.com/riywo/anyenv $install_dir
   git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
   exec $SHELL -l
-  sed -i -e 's@https://github.com/kaneshin/goenv\.git@https://github.com/syndbg/goenv\.git@' "$(ayenv root)/share/anyenv-install/goenv"
+  sed -i -e 's@https://github.com/kaneshin/goenv\.git@https://github.com/syndbg/goenv\.git@' "$(anyenv root)/share/anyenv-install/goenv"
   rbenv install $(rbenv install -l | grep -v - | tail -1)
   ndenv install $(ndenv install -l | grep '^  v' | tail -1)
   goenv install $(goenv install -l | tail -1)
