34c5f8764241868b0d7b63118afea53e8768b620
KID the Euforia <kid0725+github@gmail.com>
2017-09-04 15:34:47 +0900

    don't use diff-highlight of git/git repository
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -10,7 +10,6 @@ fi
 $script_dir/install-anyenv.sh
 $script_dir/install-bash-it.sh
 $script_dir/install-cda.sh
-$script_dir/install-diff-highlight.sh
 $script_dir/install-dircolors.sh
 $script_dir/install-enhancd.sh
 $script_dir/install-git-completion.sh
