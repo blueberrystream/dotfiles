48e4b5c90a7aff17520f2bb0c3dce0e0af2acc9a
KID the Euforia <kid0725+github@gmail.com>
2019-03-15 18:09:04 +0900

    renew install-anyenv
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/install-anyenv.sh
+++ scripts/install-anyenv.sh
@@ -3,28 +3,29 @@
 install_dir=$HOME/.anyenv
 
 if [ ! -d $install_dir ]; then
-  git clone https://github.com/riywo/anyenv $install_dir
+  git clone https://github.com/anyenv/anyenv $install_dir
   git clone https://github.com/znz/anyenv-update.git $install_dir/plugins/anyenv-update
-  sed -i -e 's@https://github.com/kaneshin/goenv\.git@https://github.com/syndbg/goenv\.git@' "$install_dir/share/anyenv-install/goenv"
 
   mkdir -p $install_dir/envs
 
   $install_dir/bin/anyenv install rbenv
-  $install_dir/bin/anyenv install ndenv
+  $install_dir/bin/anyenv install nodenv
   $install_dir/bin/anyenv install goenv
 
   rbenv=$install_dir/envs/rbenv/bin/rbenv
-  rbver=$($bin install -l | grep -v - | tail -1)
+  rbver=$($rbenv install -l | grep -v - | tail -1)
   $rbenv install $rbver
   $rbenv global $rbver
+  git clone https://github.com/rbenv/rbenv-each.git "$($rbenv root)"/plugins/rbenv-each
 
-  ndenv=$install_dir/envs/ndenv/bin/ndenv
-  ndver=$($bin install -l | grep '^  v' | tail -1)
-  $ndenv install $ndver
-  $ndenv global $ndver
+  nodenv=$install_dir/envs/nodenv/bin/nodenv
+  nodever=$($nodenv install -l | grep -v - | grep -v night | grep -v rc | tail -1)
+  $nodenv install $nodever
+  $nodenv global $nodever
+  git clone https://github.com/nodenv/nodenv-each.git "$($nodenv root)"/plugins/nodenv-each
 
   goenv=$install_dir/envs/goenv/bin/goenv
-  gover=$($bin install -l | tail -1)
+  gover=$($goenv install -l | grep -v beta | grep -v rc | tail -1)
   $goenv install $gover
   $goenv global $gover
 else
