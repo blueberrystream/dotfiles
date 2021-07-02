bdf203f892cfa4a54616134e5cd8ee82c062c69f
KID the Euforia <kid0725+github@gmail.com>
2021-07-03 01:27:02 +0900

    rubocop-airbnbをインストールできるように修正
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/gem-install.sh
+++ scripts/gem-install.sh
@@ -5,5 +5,6 @@ script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 gem update --system
 gem install bundler -v 2.2.19
 bundle install
+gem specific_install -l https://github.com/pariser/ruby.git -b ruby3  -d rubocop-airbnb
 
 rm -f Gemfile.lock
