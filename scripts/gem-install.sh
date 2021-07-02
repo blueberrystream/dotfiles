fa32b2fc31e4be544648b31f7b7d321316d573f9
KID the Euforia <kid0725+github@gmail.com>
2021-07-02 23:26:41 +0900

    improve installation gems
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/gem-install.sh
+++ scripts/gem-install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-gems=(bundler tmuxinator git-issue pry byebug pry-byebug pry-stack_explorer slim_lint sqlint neovim rubocop rubocop-rails rubocop-performance rubocop-rspec)
 
 gem update --system
-for gem in ${gems[@]}; do
-  if gem which $gem > /dev/null 2>&1; then
-    gem update $gem
-  else
-    gem install $gem
-  fi
-done
+gem install bundler -v 2.2.19
+bundle install
+
+rm -f Gemfile.lock
