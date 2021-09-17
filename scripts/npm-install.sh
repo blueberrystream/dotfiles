30016058488ebf07a69d2fb9456f7f7b581fb7cb
KID the Euforia <kid0725+github@gmail.com>
2021-09-18 04:16:16 +0900

    sort npm packages
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -2,27 +2,27 @@
 
 npm install -g npm
 npm install -g \
-  git-open \
-  yarn \
-  gtop \
-  rails-dashboard \
-  mdr \
-  npm-check-updates \
-  neovim \
-  prettier \
   @prettier/plugin-ruby \
-  eslint \
-  pug-lint \
   alex \
+  eslint \
+  eslint-config-prettier \
+  eslint-plugin-node \
+  git-open \
+  gitmoji-cli \
+  gtop \
+  heroku \
+  mdr \
+  neovim \
+  npm-check-updates \
+  prettier \
+  pug-lint \
+  rails-dashboard \
   stylelint \
   swaglint \
-  heroku \
-  gitmoji-cli \
   textlint \
   textlint-rule-preset-ja-spacing \
   textlint-rule-preset-ja-technical-writing \
   textlint-rule-spellcheck-tech-word \
-  eslint-config-prettier \
-  eslint-plugin-node \
-  tldr
+  tldr \
+  yarn
 nodenv rehash
